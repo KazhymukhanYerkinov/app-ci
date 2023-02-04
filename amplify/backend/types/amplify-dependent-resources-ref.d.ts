3a12325b8431b1cffc382fafdba6e818bc3c5043
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2023-02-05 04:35:50 +0600

    api graphql added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "appamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
   "auth": {
     "appamplifyebc49293": {
       "AppClientID": "string",
