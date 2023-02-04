21bd8d5942cce4800754ef56a4834721c272bd2d
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2023-02-05 04:19:32 +0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConf from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+Amplify.configure(awsConf);
+
+function App({ signOut }) {
   return (
-    <div className="App">
-		Welcome to Stage App 
-    </div>
+	
+	<div className="App">
+		Welcome to Amplify App 
+
+		<button onClick={signOut}> Sign Out </button>
+	</div>
+	
+	
   );
 }
 
-export default App;
+export default withAuthenticator(App);
